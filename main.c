1209d1886bb2f03f6de281cdd54d0c6e38bdd0c8
AmyJian0648945 <amychien314159@gmail.com>
2017-03-07 08:53:31 +0100

    mainfile Creation

---
+++ main.c
@@ -0,0 +1,19 @@
+/******* Header Files *******/
+
+/******* Variables *******/
+
+
+int main(){
+	printf("Hello World!");
+	return 0;
+}
+
+
+
+/***** Todo list ******
+1. Define word length (16 bit or 32 bit)
+2. 
+
+
+
+***********************/
