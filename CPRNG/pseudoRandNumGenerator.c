10e73a689e24b3b2d989db7f2cec4fcee2ea5562
AmyJian0648945 <amychien314159@gmail.com>
2017-03-20 19:56:42 +0100

    made simpleHashWithLength, in order to try and curb the memory problem

--- CPRNG/pseudoRandNumGenerator.c
+++ CPRNG/pseudoRandNumGenerator.c
@@ -4,8 +4,17 @@ What it does: generates a (pseudo) random number of length
 	seed (of any length)
 
 /// Function Description ///
-void printArray(message To Be Printed, length Of Message In uint8_t);	
-void PRNG(Pseudo Random Number, input seed, length of desired pseudo random number in uint8_t); 	
+
+void PRNG(Pseudo Random Number, input seed, 
+		  length of desired pseudo random number in uint8_t); 
+
+	This function uses A5/1 Stream Cipher to create a psuedo 
+	random number. The feedback shift register lengths are 
+	specified in the macros as "PRNG_STREAM1_LENGTH", and are 
+	created using repetitive calls to SHA256, initialised with
+	the specified input seed. "PRNG_CMP_LOCATION" is the element
+	that will be compared between the different shift registers.
+
 void RNG(Random number output, length of random number in uint8_t); 	
 	-> Doesn't require input seed
 
@@ -18,7 +27,21 @@ void RNG(Random number output, length of random number in uint8_t);
 #define RNG_Block_Length 	30 		// must be <256/8 = 32!!!
 #define RandNumLength 		60		// for testing purposes
 
+#define PRNG_FSR1_LENGTH	64
+#define PRNG_FSR2_LENGTH	65
+#define PRNG_FSR3_LENGTH	72
 
+#define PRNG_CMP_LOCATION	9		// must be < any of PRNG_FSR_LENGTH
+
+#define PRNG_XOR_LOCATION1	7		// For use in "updateFSR" must be < any of PRNG_FSR_LENGTH
+#define PRNG_XOR_LOCATION2	11			
+#define PRNG_XOR_LOCATION3	15
+#define PRNG_XOR_LOCATION4	23
+
+// Also defined in sha2.h, here for clarification and just-in-case purposes 
+#ifndef SHA256_DIGEST_LENGTH	 
+#define SHA256_DIGEST_LENGTH	32 	
+#endif /* SHA256_DIGEST_LENGTH */
 
 ////////// Include Functions //////////
 #include<stdlib.h>
@@ -31,12 +54,11 @@ void RNG(Random number output, length of random number in uint8_t);
 
 ////////// Function Declarations //////////
 
-void PRNG(uint8_t*, uint8_t*, uint32_t); 	/*Obtains pseudo random number, based on seed - last element is in BYTES */
-void RNG(uint8_t*, uint32_t); 			/* Obtains random number, length in BYTES */
+void PRNG(uint8_t*, uint8_t*, uint16_t); 	/*Obtains pseudo random number, based on seed - last element is in BYTES */
+void RNG(uint8_t*, uint16_t); 			/* Obtains random number, length in BYTES */
 
-
-
-void updateQueue(uint8_t*);
+void updateFSR(uint8_t*, uint16_t);
+uint8_t compareFSR(uint8_t*, uint8_t*, uint8_t*, uint16_t);
 
 
 
@@ -47,14 +69,14 @@ void updateQueue(uint8_t*);
 
 
 
-void RNG(uint8_t* output, uint32_t desiredOutputLength){
+void RNG(uint8_t* output, uint16_t desiredOutputLength){
 
 	// Initialise Variables
 	SHA256_CTX ctx;  	/* structure used in SHA256 */
 	static uint8_t seed[32] = {0}; 		/* this will use previous results's hash as the seed */
 	uint8_t const randString[SHA256_DIGEST_LENGTH] = "8bc73c890d2dd2977128d97ecfcdeb203ca9c27da294454595c61bb1e2684fbb";
 		/* This will be used to initialise the hash function at the very beginning - i.e. rand value that is not zero*/
-	uint32_t i = 0, j = 0, count = 0;
+	uint16_t i = 0, j = 0, count = 0;
 
 
 	// Refresh seed, or define a new one AND hash it
@@ -96,14 +118,89 @@ void RNG(uint8_t* output, uint32_t desiredOutputLength){
 
 
 
-void PRNG(uint8_t* output, uint8_t* seed, uint32_t outputLength){
+
+void updateFSR(uint8_t* output, uint16_t size){
+	uint16_t i = 0;
+	uint8_t filler = 0;
+	filler = output[PRNG_XOR_LOCATION1] ^ output[PRNG_XOR_LOCATION2] ^ output[PRNG_XOR_LOCATION3] ^ output[PRNG_XOR_LOCATION4];
+
+	for(i=0; i<size-1; i++) output[i] = output[i+1];
+	output[i] = filler;
+}
+
+
+
+uint8_t compareFSR(uint8_t* FSR1, uint8_t* FSR2, uint8_t* FSR3, uint16_t ref){
+
+	while(1){
+		if( FSR1[ref] == FSR2[ref] 	&& 	FSR1[ref] == FSR3[ref] ) {
+			printf("update all: %d %d %d ",FSR1[ref], FSR2[ref], FSR3[ref]);
+			updateFSR(FSR1, PRNG_FSR1_LENGTH); 
+			updateFSR(FSR2, PRNG_FSR2_LENGTH); 
+			updateFSR(FSR3, PRNG_FSR3_LENGTH); 
+			
+			printf("%d \n",FSR1[ref-1]);
+			return FSR1[ref-1];
+		}
+		else if( FSR1[ref] == FSR2[ref] ){ 
+
+			updateFSR(FSR1, PRNG_FSR1_LENGTH); 
+			updateFSR(FSR2, PRNG_FSR2_LENGTH); 
+		}
+		else if( FSR2[ref] == FSR3[ref] ){ 
+
+			updateFSR(FSR2, PRNG_FSR2_LENGTH); 
+			updateFSR(FSR3, PRNG_FSR3_LENGTH); 
+		}
+		else if( FSR1[ref] == FSR3[ref] ){ 
+
+			updateFSR(FSR1, PRNG_FSR1_LENGTH); 
+			updateFSR(FSR3, PRNG_FSR3_LENGTH); 
+		}
+		else{
+			printf("ERROR in pseudoRandNumGenerator.c, compareFSR: Comparison doesn't work. \n");
+			return -1;
+		}
+	}
+}
+
+
+void PRNG(uint8_t* output, uint8_t* seed, uint16_t outputLength){
+	uint8_t FSR1[PRNG_FSR1_LENGTH] = {0}, FSR2[PRNG_FSR2_LENGTH] = {0}, FSR3[PRNG_FSR3_LENGTH] = {0}; /* FSR = feedback shift registers */ 
+	uint8_t flag = 0;
+	uint16_t i = 0;
 	
-	printf("PRNG - Initial:\n"); printArray(output, outputLength);
 
-	hashOfLength(output, seed, outputLength);
+	// Generate shift registers
+	printf("FSR1///////////////:\n");
+	hashOfLength(FSR1, seed, PRNG_FSR1_LENGTH);
+	//printArray(FSR1, PRNG_FSR1_LENGTH);
 
-	printf("PRNG - After Hashing:\n"); printArray(output, outputLength);
-	// initiliase the streams with seed
+	printf("FSR2///////////////:\n");
+	hashOfLength(FSR2, FSR1, PRNG_FSR2_LENGTH);
+	//printArray(FSR2, PRNG_FSR2_LENGTH);
+
+	printf("FSR3///////////////:\n");
+	hashOfLength(FSR3, FSR2, PRNG_FSR3_LENGTH);
+	//printArray(FSR3, PRNG_FSR3_LENGTH);
+
+	
+
+	
+	
+
+	
+	
+
+	
+	
+/*
+	for(i=0; outputLength > 0; i++){
+		output[i] = compareFSR(FSR1, FSR2, FSR3, PRNG_CMP_LOCATION);
+		//printf("%x ",output[i]);
+		outputLength--;
+	}
+*/
 }
 
 
@@ -112,17 +209,16 @@ int main(){
 
 	// initialise variables
 	uint8_t pseudoRandNum[RandNumLength+10] = {0}; // first bit indicates array length, the rest are 0s
-	uint16_t numBitLength = 20; // random: 2~2048
 	uint8_t seed[36] = "1234567890abcdefghijklmnopqrstuvwxyz";	// message - to initialise some hashings
-	uint32_t i = 0;
 
 
-	
+
+	printf("PRNG - Initial:\n"); printArray(pseudoRandNum, RandNumLength);
 
 	PRNG(pseudoRandNum, seed, RandNumLength);
 	//RNG(pseudoRandNum, RandNumLength);
 
-	
+	//printf("PRNG - After Hashing:\n"); printArray(pseudoRandNum, RandNumLength);
 
 	// Print hashed message
 	
@@ -145,6 +241,24 @@ OR
 2. gcc -o outputFileName.c CNG.c
 **/
 
+
+/*
+
+		switch(compareFSR(FSR1, FSR2, FSR3, PRNG_CMP_LOCATION)) {
+	   		case 12: 	
+	      		break; 
+	   		case 23: 	
+	      		break; 
+	      	case 13: 	
+	      		break;
+	      	case 123: 	
+	      		break;
+	   		default :	
+	   			printf("ERROR in pseudoRandNumGenerator.c, PRNG: Switch statement doesn't work. \n");
+	   			break;
+		}
+
+*/
 
 
 
--- CPRNG/useSHA256.h
+++ CPRNG/useSHA256.h
@@ -2,7 +2,7 @@
 This file simply hashes the message (of any size), using sha2.h
 
 Variables: 
-	output:		contains the hashed message (of data type uint32, 256 bits in total)
+	output:		contains the hashed message (of data type uint16, 256 bits in total)
 	input:		input message 
 	lengthOfInput:
 				bit size of the input length 
@@ -29,14 +29,15 @@ To check the validity of results, compare with:
 ////////// Function Declarations //////////
 
 void simpleHash(uint8_t*, uint8_t*); 	// Does a single SHA256 hash
+void simpleHashWithLength(uint8_t*, uint8_t*, size_t); // same as simpleHash, but with specified length
 void hashWithTime(uint8_t*);	// XORs input with time data, then hashed
-void hashOfLength(uint8_t*, uint8_t*, uint32_t); /* Gives a hashed value of specified length based on seed*/ 
+void hashOfLength(uint8_t*, uint8_t*, uint16_t, size_t); /* Gives a hashed value of specified length based on seed*/ 
 
 // Helpful functions
-uint32_t timeData(); // Obtain current time to microseconds
+uint16_t timeData(); // Obtain current time to microseconds
 
-void copyArray(uint32_t*, uint32_t*, uint32_t, uint32_t); /* Copies array over from input to output, starting from "index"*/
-void printArray(uint8_t*, uint32_t);	/* prints the array */
+void copyArray(uint8_t*, uint8_t*, uint16_t, uint16_t); /* Copies array over from input to output, starting from "index"*/
+void printArray(uint8_t*, uint16_t);	/* prints the array */
 
 
 
@@ -49,7 +50,7 @@ void simpleHash(uint8_t* output, uint8_t* input){
 	// initialise variables
 	SHA256_CTX ctx;  // structure used in SHA256
 	size_t lengthOfInput = strlen(input); 
-	uint32_t i = 0;
+	printf("length = %d\n",lengthOfInput);
 
 	// Initialise SHA256	
 	SHA256_Init(&ctx); 
@@ -62,10 +63,25 @@ void simpleHash(uint8_t* output, uint8_t* input){
 }
 
 
+void simpleHashWithLength(uint8_t* output, uint8_t* input, size_t lengthOfInput){
+
+	// initialise variables
+	SHA256_CTX ctx;  // structure used in SHA256
+
+	// Initialise SHA256	
+	SHA256_Init(&ctx); 
+
+	// Input data into hash function
+	SHA256_Update(&ctx, input, lengthOfInput);
+	
+	// writes the hashing output onto output variable
+	SHA256_Final(output, &ctx); 
+}
+
 
 void hashWithTime(uint8_t* output){
 	uint8_t i = 0;
-	uint32_t timeValue = 0;
+	uint16_t timeValue = 0;
 
 	timeValue = timeData();
 	
@@ -79,25 +95,25 @@ void hashWithTime(uint8_t* output){
 }
 
 
-void hashOfLength(uint8_t* output, uint8_t* seed, uint32_t lengthOfHash){
-	uint32_t tempHashStorage[32] = {0};
-	uint32_t output_count = 0, temp_count = 0, count_ref = 0;
+void hashOfLength(uint8_t* output, uint8_t* seed, uint16_t lengthOfHash, size_t lengthOfSeed){
+	uint16_t tempHashStorage[SHA256_DIGEST_LENGTH] = {0};
+	uint16_t output_count = 0, temp_count = 0, count_ref = 0;
+
+
+	printf("simpleHash:\n"); printArray(seed, 75);
 
 	// Hash once
-	simpleHash(tempHashStorage, seed);
-
-
-
+	simpleHashWithLength(tempHashStorage, seed, lengthOfSeed);
+	//printf("simpleHash:\n"); printArray(tempHashStorage, 32);
 
 	// While lengthOfHash > SHA256_DIGEST_LENGTH, iteratively add hashed values over to output
 	count_ref = 0;
 	while (lengthOfHash > SHA256_DIGEST_LENGTH){ 
 		// Get new hash
-		simpleHash(tempHashStorage, tempHashStorage);
+		simpleHashWithLength(tempHashStorage, tempHashStorage, SHA256_DIGEST_LENGTH);
 		
 		// Copy hash array over to output array
-		//copyArray(output, tempHashStorage, count_ref, SHA256_DIGEST_LENGTH);
-
+		copyArray(output, tempHashStorage, count_ref, SHA256_DIGEST_LENGTH);
 
 		// Loop statements
 		count_ref += SHA256_DIGEST_LENGTH;
@@ -106,11 +122,8 @@ void hashOfLength(uint8_t* output, uint8_t* seed, uint32_t lengthOfHash){
 
 
 	// When lengthOfHash < SHA256_DIGEST_LENGTH, Add hashed values one last time (same operation as in while loop)
-	simpleHash(tempHashStorage, tempHashStorage);
+	simpleHash(tempHashStorage, tempHashStorage, lengthOfHash);
 	copyArray(output, tempHashStorage, count_ref, lengthOfHash);
-
-	
-	
 
 }
 
@@ -125,7 +138,7 @@ void hashOfLength(uint8_t* output, uint8_t* seed, uint32_t lengthOfHash){
 
 
 
-uint32_t timeData(){
+uint16_t timeData(){
 	struct timeval tv; /* tv is the time variable */
 
 	gettimeofday(&tv, NULL);
@@ -133,31 +146,24 @@ uint32_t timeData(){
 }
 
 
-void copyArray(uint32_t* output, uint32_t* input, uint32_t startingIndex, uint32_t lengthToCopy){
-	uint32_t i = 0;
+void copyArray(uint8_t* output, uint8_t* input, uint16_t startingIndex, uint16_t lengthToCopy){
+	uint16_t i = 0;
 
-	printf("DEBUG: %x  %d\n\n",startingIndex + lengthToCopy, &output[startingIndex + lengthToCopy ]);
-
-	/*
 	for(i = 0; i < lengthToCopy; i++) {
-		printf("%d ",startingIndex + i);
 		output[startingIndex + i] = input[i];
-	}*/
-	//printf("somt %x\n", output[startingIndex]);
+	}
 }
 
 
-void printArray(uint8_t* output, uint32_t iter){
-	uint32_t i=0;
+void printArray(uint8_t* output, uint16_t iter){
+	uint16_t i = 0;
 
 	for(i = 0; i < iter; i++) {
-
 		printf("%x\t", output[i]);
 
 		if(i%5 == 4) 	printf("\n");
 		if(i%25 == 24) 	printf("\n");
 	}
-	printf("\n\n");
 }
 
 
