7346a1a927c753a956efc5a02cb50099b5a69536
AmyJian0648945 <amychien314159@gmail.com>
2017-04-01 22:42:54 +0200

    fixed the mem leak)

--- aes/useAES.h
+++ aes/useAES.h
@@ -66,6 +66,7 @@ void aesCBCencrypt(uint8_t* ciphertext, uint8_t* inputMsg, uint8_t* msgLength,
              uint8_t* IV, uint8_t* key)
 {
     uint8_t temp[aes_BLOCK_SIZE] = {0};
+    uint8_t tempXOR[aes_BLOCK_SIZE] = {0};
     uint8_t checkingArray[aes_BLOCK_SIZE] = {0};
     uint8_t CBCrounds = 0;
     uint16_t i = 0;
@@ -80,11 +81,11 @@ void aesCBCencrypt(uint8_t* ciphertext, uint8_t* inputMsg, uint8_t* msgLength,
     // Go through all rounds AES, each with input 128bits of message
     for(i = 0; i < CBCrounds; i++){
         // XOR previous ciphertext with current plaintext (unless its the first iteration)  
-        XOR(inputMsg + i*(aes_BLOCK_SIZE), temp, inputMsg + i*(aes_BLOCK_SIZE), aes_BLOCK_SIZE);
-
-        // Compute a single AES 
-        aesEncrypt(ciphertext + i*(aes_BLOCK_SIZE), inputMsg + i*(aes_BLOCK_SIZE), key);
+        XOR(tempXOR, temp, inputMsg + i*(aes_BLOCK_SIZE), aes_BLOCK_SIZE);
         
+        // Compute a single AES 
+        aesEncrypt(ciphertext + i*(aes_BLOCK_SIZE), tempXOR, key);
+
         // Copy the ciphertext onto "temp", to calculate XOR for next iteration
         copyArrayFrom0(temp, ciphertext + i*(aes_BLOCK_SIZE), aes_BLOCK_SIZE);   
     }
@@ -94,11 +95,13 @@ void aesCBCencrypt(uint8_t* ciphertext, uint8_t* inputMsg, uint8_t* msgLength,
 void padding(uint8_t* inputToBePadded, uint8_t* inputLength){
     uint8_t pad = 0, i = 0;
 
+    // figure out 
     pad = *inputLength;
     pad = 16 - (pad % 16);
 
+    // pad as many times as needed
     for(i = *inputLength; i < pad + *inputLength; i++) inputToBePadded[i] = pad;
-        
+
     // update message length    
     *inputLength += pad;
 }
