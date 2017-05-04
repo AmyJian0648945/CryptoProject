1c06b50fed394e07f203ad31d3d422794f76b56d
AmyJian0648945 <amychien314159@gmail.com>
2017-05-04 12:22:52 +0200

    updating master with the code that halved the time - 1

---
+++ crypto_dspSimulation/Debug/Key_Establishment/Source/modularInverse32.pp
@@ -0,0 +1,19 @@
+# FIXED
+
+Key_Establishment/Source/modularInverse32.obj: ../Key_Establishment/Source/modularInverse32.c
+Key_Establishment/Source/modularInverse32.obj: ../Key_Establishment/Source/../Header/modularInverse32.h
+Key_Establishment/Source/modularInverse32.obj: ../Key_Establishment/Source/../Header/additionalFunctions32.h
+Key_Establishment/Source/modularInverse32.obj: C:/ti/ccsv5/tools/compiler/c6000/include/stdint.h
+Key_Establishment/Source/modularInverse32.obj: C:/ti/ccsv5/tools/compiler/c6000/include/stdio.h
+Key_Establishment/Source/modularInverse32.obj: C:/ti/ccsv5/tools/compiler/c6000/include/linkage.h
+Key_Establishment/Source/modularInverse32.obj: C:/ti/ccsv5/tools/compiler/c6000/include/stdarg.h
+Key_Establishment/Source/modularInverse32.obj: C:/ti/ccsv5/tools/compiler/c6000/include/string.h
+
+../Key_Establishment/Source/modularInverse32.c: 
+../Key_Establishment/Source/../Header/modularInverse32.h: 
+../Key_Establishment/Source/../Header/additionalFunctions32.h: 
+C:/ti/ccsv5/tools/compiler/c6000/include/stdint.h: 
+C:/ti/ccsv5/tools/compiler/c6000/include/stdio.h: 
+C:/ti/ccsv5/tools/compiler/c6000/include/linkage.h: 
+C:/ti/ccsv5/tools/compiler/c6000/include/stdarg.h: 
+C:/ti/ccsv5/tools/compiler/c6000/include/string.h: 
---
+++ crypto_dspSimulation/Release/Key_Establishment/Source/montExponentiation.pp
@@ -0,0 +1,5 @@
+# FIXED
+
+Key_Establishment/Source/montExponentiation.obj: ../Key_Establishment/Source/montExponentiation.c
+
+../Key_Establishment/Source/montExponentiation.c: 
